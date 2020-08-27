9e394567ff20207f7fefcc107d434a776117f97b
yeol.you <vflag21c@nate.com>
2020-08-27 22:29:39 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/hello/hello.java
@@ -0,0 +1,12 @@
+package com.example.demo.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+    @GetMapping("/")
+    public String hello() {
+        return "양태영 바보";
+    }
+}
