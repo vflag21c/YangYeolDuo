b5f08396a0c70aebf5342fda9b5b04002bb0db2c
yeol.you <vflag21c@nate.com>
2020-08-31 23:06:32 +0900

    푸시 적용 테스트 2

--- src/main/java/com/example/demo/hello/hello.java
+++ src/main/java/com/example/demo/hello/hello.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class hello {
     @GetMapping("/")
     public String hello() {
-        return "적용 됐따 양태영 바보야";
+        return "울지마 바보야 나정말 괜찮아";
     }
 
     @GetMapping("/test")
